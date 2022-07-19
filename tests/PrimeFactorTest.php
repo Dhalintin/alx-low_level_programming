86b9d1d94972ea727db1929ce200bb1ecbeee609
Dhalintin <99981753+Dhalintin@users.noreply.github.com>
2022-07-20 02:48:44 +0300

    Update PrimeFactorTest.php

--- tests/PrimeFactorTest.php
+++ tests/PrimeFactorTest.php
@@ -11,12 +11,13 @@ use PHPUnit\Framework\TestCase;
 	 * @dataProvider factors
 	 * @param $number
 	 * @param $expected
-	 /
+	 */
 	 public function generates_prime_factors_of_number($number, $expected){
 		 $factor = new PrimeFactors;
-		 $this >assertEquals($expected, $factor >generate($number));
-		 }
-		 public function factors(){
+		 $this->assertEquals([$expected], $factor->generate($number));
+		}
+		
+	public function factors(){
 			 return[
 			 [1, []],
 			 [2, [2]],
