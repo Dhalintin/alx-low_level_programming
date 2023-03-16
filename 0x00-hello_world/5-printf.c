3661f5968a8316cd4b3e9890503207b007b22a57
Dhalintin <uchexdhalitin@gmail.com>
2023-03-16 20:43:26 +0100

    Fix: corrected some error

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /*
-*This is documentation
+*The main function is to print out a string
 */
 
-int main (void)
+int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-
-    return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return(0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 
-int main (void)
-{
-    char c;
-    int i;
-    long int li;
-    long long int lli;
-    float f;
+/*
+*The main function is to print out a string
+*/
 
-    printf("Size of a char: %lu byte(s)", (unsigned long)sizeof(c));
-    printf("Size of an int: %lu byte(s)" (unsigned long)sizeof(i));
-    printf("Size of a long int: %lu byte(s)", (unsigned long)sizeof(li));
-    printf("Size of a long long int: %lu byte(s)", (unsigned long)sizeof(lli));
-    printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(f));
-    
-    return (0);
+int main(void)
+{
+char c;
+int i;
+long int li;
+long long int lli;
+float f;
+
+printf("Size of a char: %lu byte(s)", (unsigned long)sizeof(c));
+printf("Size of an int: %lu byte(s)", (unsigned long)sizeof(i));
+printf("Size of a long int: %lu byte(s)", (unsigned long)sizeof(li));
+printf("Size of a long long int: %lu byte(s)", (unsigned long)sizeof(lli));
+printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(f));
+
+return(0);
 }
