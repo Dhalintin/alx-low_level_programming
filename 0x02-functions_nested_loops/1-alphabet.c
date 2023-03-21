374e2db34859611be80c92bfecd3146f7ea5cf2b
Dhalintin <uchexdhalitin@gmail.com>
2023-03-21 15:31:33 +0100

    Chore: Nested Loops and Functions in C

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,12 +4,13 @@
  * Description - prints the alphabet in lowercase, followed by new line.
  * Return: Always 0 (success)
  */
-
-void print_alphabet(void){
-char letter;
-for(letter = 'a'; letter<= 'z'; letter++)
+void print_alphabet(void)
 {
-_putchar(letter);
-}
-_putchar('/n');
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
 }
