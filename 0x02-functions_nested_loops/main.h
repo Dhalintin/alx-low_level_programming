00658ba914f93afb8f189cf8fe6683f750c8e4ad
Dhalintin <uchexdhalitin@gmail.com>
2023-03-21 14:34:35 +0100

    Chore: Nested Loops and Functions in C

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int sign(int c);
+int _abs(int c);
+void jack_bauer(void);
+void times_table(void);
+void print_to_98(int n);
