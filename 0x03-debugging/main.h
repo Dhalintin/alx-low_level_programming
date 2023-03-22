6ad68ef3245de6263cdd3aa1f8b6771d4eb595f5
Dhalintin <uchexdhalitin@gmail.com>
2023-03-22 17:41:58 +0100

    Chore: C-Debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,5 +3,4 @@
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 int largest_number(int a, int b, int c);
-int largest_number(int a, int b, int c);
 void positive_or_negative(int i);
