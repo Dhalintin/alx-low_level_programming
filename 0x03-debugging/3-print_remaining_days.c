9495da23139d9952770f946bbc9481dc63fc2192
Dhalintin <uchexdhalitin@gmail.com>
2023-03-22 16:34:54 +0100

    Chore: C-Debugging

---
+++ 0x03-debugging/3-print_remaining_days.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+int month  = 2;
+int day = 29;
+int year = 2000;
+print_remaining_days(month, day, year);
+
+return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+#This project is meant to learn more about debugging
