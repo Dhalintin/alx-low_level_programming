1c1cf6af96d9228eb73e684efb95b9941c537127
Dhalintin <uchexdhalitin@gmail.com>
2023-03-23 17:34:58 +0100

    Chore: More on functions and Nested Loops in C

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,30 +1,29 @@
+
 #include "main.h"
-
-
 /**
- * print_triangle - Prints a triangle, using the character #.
- * @size: The size of the triangle.
- */
+* print_triangle - Prints a triangle, using the character #.
+* @size: The size of the triangle.
+*/
 void print_triangle(int size)
 {
-int i, j;
-if(size >= 0){
-for (i = 1; i <= size; i++)
-{
-for (j = size - i; j > 0; j--)
-{
-_putchar(' ');
-}
+int hash, index;
 
-for (j = 0; j < i; j++)
+if (size > 0)
 {
+for (hash = 1; hash <= size; hash++)
+{
+for (index = size - hash; index > 0; index--)
+_putchar(' ');
+
+for (index = 0; index < hash; index++)
 _putchar('#');
-}
-if (i == size)
-{		continue;
-}
+
+if (hash == size)
+continue;
+
 _putchar('\n');
 }
 }
+
 _putchar('\n');
 }
