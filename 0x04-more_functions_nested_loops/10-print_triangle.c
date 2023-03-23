9054a736f769d136b67c50f59f6816678d65d92d
Dhalintin <uchexdhalitin@gmail.com>
2023-03-23 17:31:19 +0100

    Chore: More on functions and Nested Loops in C

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,14 +10,18 @@ void print_triangle(int size)
 {
 int i, j;
 if(size >= 0){
-for(i = 1; i <= size; i++){
-for(j = 1; j <= size; j++){
-if((i + j) >= size){
-_putchar('#');
-}else{
+for (i = 1; i <= size; i++)
+{
+for (j = size - i; j > 0; j--)
 _putchar(' ');
-}
-}
+
+for (j = 0; j < i; j++)
+_putchar('#');
+
+if (i == size)
+continue;
+
+_putchar('\n');
 }
 }
 _putchar('\n');
