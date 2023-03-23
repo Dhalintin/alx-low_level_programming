cc488ab4c564504855741d678efe12fd65e30df6
Dhalintin <uchexdhalitin@gmail.com>
2023-03-23 17:17:42 +0100

    Chore: More on functions and Nested Loops in C

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * _isupper - Checks for uppercase characters.
  * @c: The character to be checked.
@@ -12,8 +11,11 @@ int _isupper(int c)
 if (c >= 'A' && c <= 'Z')
 {
 return (1);
+
 }else
 {
+
 return (0);
 }
+
 }
