a91b48d0f1c59da5e32954309169a9b94a509f67
Dhalintin <uchexdhalitin@gmail.com>
2023-03-23 18:29:36 +0100

    Chore: More on functions and Nested Loops in C

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-* main - check the code.
-*@a: first value
-*@b: second value
-* Return: the product of the two values.
+ * mul - Multiples two integers.
+ * @a: The first integer to be multiplied.
+ * @b: The second integer to be multiplied.
+ *
+ * Return: The result of the multiplication.
 */
 int mul(int a, int b)
 {
 
 return (a * b);
-
 }
