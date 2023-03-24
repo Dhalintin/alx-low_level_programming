fa6e72de7a13f257fa7f8aeb6bfe2a1a4f5a9364
Dhalintin <uchexdhalitin@gmail.com>
2023-03-24 11:03:39 +0100

    Optional task

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -3,22 +3,18 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer.
- * @n: The integer to be printed.
- */
+* print_number - Prints an integer.
+* @n: The integer to be printed.
+*/
 void print_factor()
 {
-	long num = 612852475143;
-    int i;
-	for(i = 2; i < sqrt(num); i++){
-        while (num % i == o){
-            num /= i;
-        }
-    }
+long num = 612852475143;
+int i;
+for(i = 2; i < sqrt(num); i++){
+while (num % i == o){
+num /= i;
+}
+}
 
-    if(i > num){
-        _putchar(i);
-    }else{
-        _putchar(num);
-    }
+_putchar(num);
 }
