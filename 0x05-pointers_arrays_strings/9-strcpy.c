986ae831a10dde6c2407795407e5b431996d4ec0
Dhalintin <uchexdhalitin@gmail.com>
2023-03-27 18:52:57 +0100

    Pointers, arrays and string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,17 +2,20 @@
 
 /**
 *This function takes a ponter n
-* @*n
+* @_strcpy
+*@dest
+@src
 *and Updates the value to 98 
 */
 
 char *_strcpy(char *dest, char *src)
 {
 int i = 0;
-while (src[i] != '\0') {
+while (src[i] != '\0')
+{
 dest[i] = src[i];
 i++;
 }
 dest[i] = '\0';
-return dest;
+return (dest);
 }
