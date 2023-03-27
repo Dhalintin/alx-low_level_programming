38afa222bd22449538b21447eeec8cc250008c1a
Dhalintin <uchexdhalitin@gmail.com>
2023-03-27 19:25:10 +0100

    Pointers, arrays and string

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- * reset_to_98 - takes a pointer to an int as peremeter
- * @n: pointer to n
- *
-
+* reset_to_98 - takes a pointer to an int as peremeter
+* @n: pointer to n
+*/
 void reset_to_98(int *n)
 {
 *n = 98;
