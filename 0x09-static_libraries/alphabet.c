32b25a6503c1a1ed07784cfc853863af99432812
Dhalintin <uchexdhalitin@gmail.com>
2023-04-08 15:27:32 +0100

    Static libraries

---
+++ 0x09-static_libraries/alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ * Description - prints the alphabet in lowercase, followed by new line.
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+}
---
+++ 0x09-static_libraries/print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ * Description - prints 10 times the alphabet
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int count = 1;
+	char letter;
+
+	while (count++ <= 10)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			putchar(letter);
+		}
+		putchar('\n');
+	}
+}
