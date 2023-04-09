ace4af9bf71b3222c050ae70494bb272dbebe4cc
Dhalintin <uchexdhalitin@gmail.com>
2023-04-09 20:44:48 +0100

    Static libraries

---
+++ 0x09-static_libraries/c files/100-atoi.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int _atoi(char *str)
+{
+    return atoi(str);
+}
---
+++ 0x09-static_libraries/c files/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stddef.h>
+#include <string.h>
+/**
+ *_strlen - get the length of a string
+ *@s: string pointer to pass to this function
+ * Return: The length of *s
+ */
+int _strlen(char *s)
+{
+return (strlen(s));
+
+}
