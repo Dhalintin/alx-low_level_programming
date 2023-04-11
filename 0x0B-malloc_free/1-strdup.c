59d0a38726053b8053712fc041b621deacba64d1
Dhalintin <uchexdhalitin@gmail.com>
2023-04-11 15:24:19 +0100

    Malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,34 @@
 #include "main.h"
+#include <stdlib.h>
+/**
+* *_strdup - function that returns a pointer to a newly allocated space
+* in memory, which contains a copy of the string given as a parameter.
+* @str: string
+* Return: NULL
+*/
+char *_strdup(char *str)
+{
+unsigned int i, j = 0;
+char *copy;
 
+if (str == NULL)
+return (NULL);
 
+for (i = 0; str[i]; i++)
+;
+i++;
 
-char *_strdup(char *str);
+copy = malloc(i * sizeof(char));
+
+if (copy == NULL)
+{
+return (NULL);
+}
+
+for (j = 0; j < i; j++)
+{
+copy[j] = str[j];
+}
+
+return (copy);
+}
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,38 @@
 #include "main.h"
+#include <stdlib.h>
+/**
+* str_concat - function that cincatenates two strings.
+* @s1: first string
+* @s2: second string
+* Return: result or NULL on failure.
+*/
+char *str_concat(char *s1, char *s2)
+{
+unsigned int a, b, c, l;
+char *s;
 
-
-
-char *str_concat(char *s1, char *s2);
+if (s1 == NULL)
+a = 0;
+else
+{
+for (a = 0; s1[a]; a++)
+;
+}
+if (s2 == NULL)
+b = 0;
+else
+{
+for (b = 0; s2[b]; b++)
+;
+}
+c = a + b + 1;
+s = malloc(c * sizeof(char));
+if (s == NULL)
+return (NULL);
+for (l = 0; l < a; l++)
+s[l] = s1[l];
+for (l = 0; l < b; l++)
+s[l + a] = s2[l];
+s[a + b] = '\0';
+return (s);
+}
