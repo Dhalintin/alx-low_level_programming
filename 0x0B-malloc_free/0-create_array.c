f7498b1a69e592a6fb34948a69440af9a78430fb
Dhalintin <uchexdhalitin@gmail.com>
2023-04-11 13:10:18 +0100

    Malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+*create_array is a function that 
+*@size: size of the array
+*@c: char to iniitialize the array
+*Return: Pointer or Null
+*/
+char *create_array(unsigned int size, char c)
+{
+unsigned int i;
+char *a;
+
+
+if(size === 0){
+return NULL;
+
+a = malloc(size * sizeof(c))
+
+if(a == NULL)
+{
+    return NULL;
+}
+
+for(i = 0; i < size;i++)
+{
+a[i] = c;
+}
+return (a);
+}
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,4 @@
+#include "main.h"
+
+
+void free_grid(int **grid, int height);
