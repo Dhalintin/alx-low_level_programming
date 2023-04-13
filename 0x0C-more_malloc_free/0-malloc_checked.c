4ea823fe3d694768f5d0ed82cdb4f1c50dab8633
Dhalintin <uchexdhalitin@gmail.com>
2023-04-13 17:08:21 +0100

    More macloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* malloc_unchecked: is a function that takes an integer 
+* @b: integer value and
+* Return: pointer 
+*/
+
+void *malloc_checked(unsigned int b)
+{
+int *c
+c = malloc(b)
+if(c == null)
+exit(98);
+
+return (*b)
+}
